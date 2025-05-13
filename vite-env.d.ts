01d705c336537def8cda553e42626e389e32f883
DJIBEYROU <69572984+DJIBEYROU@users.noreply.github.com>
2025-05-13 12:20:27 +0900

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  // Add other environment variables here if you have them, for example:
+  // readonly VITE_APP_TITLE: string
+  // ...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
